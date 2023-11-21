fb2e57a90bc6b1c59f3060c4ff11e2411534867c
Kyle Sandell <kasandell@users.noreply.github.com>
2023-11-20 22:31:09 -0800

    initial setup

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
+#[macro_use]
+extern crate log;
+extern crate num;
+#[macro_use]
+extern crate num_derive;
+extern crate env_logger;
+
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+use dotenv::dotenv;
+
+mod util;
+mod api_error;
+mod constant;
+mod card;
+mod category;
+mod membership;
+mod transaction;
+mod user;
+mod wallet;
+mod middleware;
+
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    //let pool = util::db::establish_connection();
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    util::db::init();
+
+    /*
+    let state = {
+        let pool = util::db::establish_connection();
+        use middleware::state::AppState;
+        AppState::new(pool)
+    };
+    */
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            //.wrap(api_key_validation::ApiKeyValidation)
+            //.app_data(web::Data::new(state.clone()))
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
---
+++ src/util/db.rs
@@ -0,0 +1,39 @@
+use crate::constant::env_key;
+use crate::api_error::ApiError;
+use diesel::pg::PgConnection;
+use diesel::r2d2::{ConnectionManager};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use dotenv::dotenv;
+use std::env;
+use lazy_static::lazy_static;
+
+type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+pub type DbConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
+type DB = diesel::pg::Pg;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+
+
+fn run_migration(conn: &mut impl MigrationHarness<DB>) {
+    conn.run_pending_migrations(MIGRATIONS).unwrap();
+}
+
+lazy_static! {
+    static ref POOL: Pool = {
+        let db_url = env::var("DATABASE_URL").expect("Database url not set");
+        let manager = ConnectionManager::<PgConnection>::new(db_url);
+        Pool::new(manager).expect("Failed to create db pool")
+    };
+}
+
+pub fn init() {
+    info!("Initializing DB");
+    lazy_static::initialize(&POOL);
+    let mut conn = connection().expect("Failed to get db connection");
+    run_migration(&mut conn);
+}
+
+pub fn connection() -> Result<DbConnection, ApiError> {
+    POOL.get()
+        .map_err(|e| ApiError::new(500, format!("Failed getting db connection: {}", e)))
+}
