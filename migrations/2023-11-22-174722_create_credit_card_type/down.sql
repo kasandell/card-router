a3c1878c9a919f848029c7f20ed10b5b51a5743c
Kyle Sandell <kasandell@users.noreply.github.com>
2023-11-25 15:04:12 -0700

    adjust unique constraints

--- migrations/2023-11-22-174722_create_credit_card_type/down.sql
+++ migrations/2023-11-22-174722_create_credit_card_type/down.sql
@@ -1,4 +1,4 @@
--- This file should undo anything in `up.sql`
+DROP INDEX IF EXISTS unique_card_name_issuer_type;
 DROP TABLE IF EXISTS credit_card;
 DROP TABLE IF EXISTS credit_card_type;
 DROP TABLE IF EXISTS credit_card_issuer;
--- migrations/2023-11-22-174722_create_credit_card_type/up.sql
+++ migrations/2023-11-22-174722_create_credit_card_type/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS credit_card_issuer (
     id SERIAL PRIMARY KEY,
     public_id UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
-    "name" VARCHAR(255) NOT NULL, -- Bilt, Chase, Etc.
+    "name" VARCHAR(255) UNIQUE NOT NULL, -- Bilt, Chase, Etc.
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS credit_card_issuer (
 CREATE TABLE IF NOT EXISTS credit_card_type (
     id SERIAL PRIMARY KEY,
     public_id UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
-    "name" VARCHAR(255) NOT NULL, -- Visa, Mastercard, Amex
+    "name" VARCHAR(255) UNIQUE NOT NULL, -- Visa, Mastercard, Amex
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
@@ -17,10 +17,12 @@ CREATE TABLE IF NOT EXISTS credit_card_type (
 CREATE TABLE IF NOT EXISTS credit_card (
     id SERIAL PRIMARY KEY,
     public_id UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
-    "name" VARCHAR(255) NOT NULL, -- Sapphire Reserve, etc.,
+    "name" VARCHAR(255) UNIQUE NOT NULL, -- Sapphire Reserve, etc.,
     credit_card_type_id INT NOT NULL REFERENCES credit_card_type(id),
     credit_card_issuer_id INT NOT NULL REFERENCES credit_card_issuer(id),
     card_image_url VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
+
+CREATE UNIQUE INDEX IF NOT EXISTS unique_card_name_issuer_type ON credit_card("name", credit_card_type_id, credit_card_issuer_id);
