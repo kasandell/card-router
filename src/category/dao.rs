4302a799e523def435522d257c32e472a0f8b660
Kyle Sandell <kasandell@users.noreply.github.com>
2024-02-22 22:44:11 -0500

    fix compilation errors

--- src/category/dao.rs
+++ src/category/dao.rs
@@ -6,15 +6,11 @@ use mockall::{automock, predicate::*};
 #[cfg_attr(test, automock)]
 pub trait CategoryDaoTrait {
     fn create(&self, category: InsertableCategory) -> Result<Category, DataError>;
-    fn delete_by_id(&self, id: i32) -> Result<usize, DataError>;
-    fn delete(&self, category: &Category) -> Result<usize, DataError>;
 }
 
 #[cfg_attr(test, automock)]
 pub trait MccMappingDaoTrait {
     fn create(&self, mapping: InsertableMccMapping) -> Result<MccMapping, DataError>;
-    fn delete_by_id(&self, id: i32) -> Result<usize, DataError>;
-    fn delete(&self, mapping: &MccMapping) -> Result<usize, DataError>;
 }
 
 pub struct CategoryDao{}
@@ -32,14 +28,6 @@ impl CategoryDaoTrait for CategoryDao {
     fn create(&self, category: InsertableCategory) -> Result<Category, DataError> {
         Category::create(category)
     }
-
-    fn delete_by_id(&self, id: i32) -> Result<usize, DataError> {
-        Category::delete(id)
-    }
-
-    fn delete(&self, category: &Category) -> Result<usize, DataError> {
-        category.delete_self()
-    }
 }
 
 impl MccMappingDao {
@@ -51,13 +39,5 @@ impl MccMappingDao {
 impl MccMappingDaoTrait for MccMappingDao {
     fn create(&self, mapping: InsertableMccMapping) -> Result<MccMapping, DataError> {
         MccMapping::create(mapping)
-    }
-
-    fn delete_by_id(&self, id: i32) -> Result<usize, DataError> {
-        MccMapping::delete(id)
-    }
-
-    fn delete(&self, mapping: &MccMapping) -> Result<usize, DataError> {
-        mapping.delete_self()
     }
 }
