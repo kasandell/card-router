2a0764ccc5adaee708fdf9b05ee26b0d0cd5ce4d
Kyle Sandell <kasandell@users.noreply.github.com>
2024-03-20 22:03:33 -0400

    more test coverage

--- src/user/entity_tests.rs
+++ src/user/entity_tests.rs
@@ -1,8 +1,9 @@
 #[cfg(test)]
 mod test {
+    use uuid::Uuid;
     use crate::error::error_type::ErrorType;
     use crate::user::dao::{UserDao, UserDaoTrait};
-    use crate::user::entity::UserMessage;
+    use crate::user::entity::{User, UserMessage};
 
     pub const EMAIL: &str = "test@email.com";
     pub const EMAIL2: &str = "test2@email.com";
@@ -15,6 +16,21 @@ mod test {
     async fn test_create() {
         let dao = UserDao::new();
         let user = dao.create(
+            &UserMessage {
+                email: EMAIL,
+                auth0_user_id: AUTH0_ID,
+                footprint_vault_id: FOOTPRINT_ID,
+            }
+        ).await.expect("create user");
+        assert_eq!(user.email.as_str(), EMAIL);
+        assert_eq!(user.auth0_user_id.as_str(), AUTH0_ID);
+        assert_eq!(user.footprint_vault_id.as_str(), FOOTPRINT_ID);
+        user.delete_self().await.expect("deletes");
+    }
+
+    #[actix_web::test]
+    async fn test_create_from_entity() {
+        let user = User::create(
             &UserMessage {
                 email: EMAIL,
                 auth0_user_id: AUTH0_ID,
@@ -92,10 +108,26 @@ mod test {
 
     #[actix_web::test]
     async fn test_find_uuid_finds() {
+        let dao = UserDao::new();
+        let user = dao.create(
+            &UserMessage {
+                email: EMAIL,
+                auth0_user_id: AUTH0_ID,
+                footprint_vault_id: FOOTPRINT_ID,
+            }
+        ).await.expect("create user");
+
+        let found = dao.find(&user.public_id).await.expect("Should find");
+        assert_eq!(found, user);
+        user.delete_self().await.expect("deletes");
     }
 
     #[actix_web::test]
-    async fn test_find_uuid_does_not_find() {}
+    async fn test_find_uuid_does_not_find() {
+        let dao = UserDao::new();
+        let found_err = dao.find(&Uuid::new_v4()).await.expect_err("Should not find");
+        assert_eq!(found_err.error_type, ErrorType::NotFound);
+    }
 
     #[actix_web::test]
     async fn test_find_email_finds() {
